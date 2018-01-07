ed8274a2d846d37c145d391ce0d70d73654c64f4
Juan <juan@dreamvent.com>
2018-01-06 22:36:29 -0800

    tuned prediction and data selection

--- YFMachLearn.py
+++ YFMachLearn.py
@@ -1,4 +1,7 @@
 import csv
+import sklearn.svm as SVR
+import matplotlib.pyplot as plt
+import numpy as np
 class stock():
     date = ""
     open = 0
@@ -17,17 +20,42 @@ class stock():
         newdate = date[:4]
         newdate += date[5:7]
         newdate += date[8:]
-        print (newdate)
         return newdate
+
+def predict_prices(dates, price, x):
+    dates = np.reshape(dates,(len(dates),1))
+
+    svr_lin = SVR.SVR(kernel = 'linear', C=1e3)
+    # svr_poly = SVR.SVR(kernel = 'poly', C=1e3, degree = 2)
+    svr_rbf = SVR.SVR(kernel = 'rbf', C=1e3, gamma = 0.1)
+    svr_lin.fit(dates,price)
+    # svr_poly.fit(dates,price)
+    svr_rbf.fit(dates,price)
+    plt.scatter(dates,price, color='black', label = 'Data')
+    plt.plot(dates,svr_rbf.predict(dates), color = 'red', label='RBF model')
+    plt.plot(dates, svr_lin.predict(dates), color='green', label='Linear Model')
+    # plt.plot(dates, svr_poly.predict(dates), color='blue', label='Polynomial Model')
+    plt.tick_params(direction='out', length=1, width=1, colors='r')
+    plt.xlabel('days')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.show()
+    return  svr_lin.predict(x)[0], svr_rbf.predict(x)[0]#  svr_poly.predict(x)[0]
 
 def parseCSV():
     prices = []
     with open('AAL.csv', newline='') as data:
         reader = csv.DictReader(data)
         for row in reader:
-            prices.append(stock(row['Date'], row['Open'], row['High'], row['Low'], row['Close'], row['Volume']))
-
+            prices.append(stock(row['Date'], row['Open'], row['High'], row['Low'], round(float(row['Close']),2), row['Volume']))
     return prices
 stocks = parseCSV()
+dates = [x.date for x in stocks[0:100]]
+prices= [x.prices for x in stocks[0:100]]
+for item in range(100):
+    dates.append(item)
+    prices.append(stocks[item+1500].close)
+predict = predict_prices(dates,prices,100)
+print (predict)
 
 
